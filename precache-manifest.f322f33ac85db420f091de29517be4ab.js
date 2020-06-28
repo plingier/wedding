b0e54b7a16d8e1d515310b13de288d7ce9d3eef3
Plingier <bananapro@mbp-van-banana.home>
2020-06-28 11:35:18 +0200

    Updates

---
+++ precache-manifest.f322f33ac85db420f091de29517be4ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ae8e1c1d0be75657b39d14c82e14ad",
+    "url": "/wedding/index.html"
+  },
+  {
+    "revision": "33d37df61ed7e8ab2921",
+    "url": "/wedding/static/css/main.cf1a155a.chunk.css"
+  },
+  {
+    "revision": "bbab4ea9ac8137f2a1ca",
+    "url": "/wedding/static/js/2.35aec7b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wedding/static/js/2.35aec7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d37df61ed7e8ab2921",
+    "url": "/wedding/static/js/main.d1f8a17c.chunk.js"
+  },
+  {
+    "revision": "8d3e3f34c14f9ad645c5",
+    "url": "/wedding/static/js/runtime-main.0bb40c67.js"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/wedding/static/media/hero.6e09f410.jpg"
+  }
+]);
