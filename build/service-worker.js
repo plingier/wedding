ac8ed88559848f72bbfd1797b477cf2288546059
Plingier <bananapro@MacBook-Pro-van-Banana.local>
2020-06-29 10:48:00 +0200

    fixing breakpoints

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wedding/precache-manifest.f322f33ac85db420f091de29517be4ab.js"
+  "/wedding/precache-manifest.0922e0e9084c7cf0a637d8f79c082290.js"
 );
 
 self.addEventListener('message', (event) => {
