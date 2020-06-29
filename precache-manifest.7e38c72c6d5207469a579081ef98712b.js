5a62bbd21959aa3c59095c566d52142a8f402ec6
Plingier <bananapro@MacBook-Pro-van-Banana.local>
2020-06-29 15:06:51 +0200

    Updates

---
+++ precache-manifest.7e38c72c6d5207469a579081ef98712b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcf58b5f9327ac70607ad6359ee40da",
+    "url": "/wedding/index.html"
+  },
+  {
+    "revision": "2c800e7e5da6ee92bb58",
+    "url": "/wedding/static/css/main.1283ed52.chunk.css"
+  },
+  {
+    "revision": "9d3e6501fc16387c1e70",
+    "url": "/wedding/static/js/2.0421a032.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wedding/static/js/2.0421a032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c800e7e5da6ee92bb58",
+    "url": "/wedding/static/js/main.35e69b70.chunk.js"
+  },
+  {
+    "revision": "8d3e3f34c14f9ad645c5",
+    "url": "/wedding/static/js/runtime-main.0bb40c67.js"
+  },
+  {
+    "revision": "94939136c88915eb8c333e1a5e876f8d",
+    "url": "/wedding/static/media/hero-small.94939136.jpg"
+  },
+  {
+    "revision": "171a43bfffe02e98625c9231abdd6954",
+    "url": "/wedding/static/media/test.171a43bf.jpg"
+  }
+]);
